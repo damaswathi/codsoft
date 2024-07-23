fee3cd5b3b8b8b14ffc95ec301d0ad5c74872f16
damaswathi <damaswathichowdary@gmail.com>
2024-07-24 00:17:24 +0530

    task1.py

--- task1.py
+++ task1.py
@@ -1,51 +1,57 @@
-class ToDoList:
-    def _init_(self):
-        self.tasks = []
+tasks = []
 
-    def add_task(self, task):
-        self.tasks.append(task)
-        print(f"Added task: {task}")
 
-    def view_tasks(self):
-        if not self.tasks:
-            print("No tasks in the list.")
-        else:
-            print("To-Do List:")
-            for index, task in enumerate(self.tasks, start=1):
-                print(f"{index}. {task}")
+def addTask():
+  task = input("Please enter a task: ")
+  tasks.append(task)
+  print(f"Task '{task}' added to the list.")
 
-    def delete_task(self, task_number):
-        if 0 < task_number <= len(self.tasks):
-            removed_task = self.tasks.pop(task_number - 1)
-            print(f"Deleted task: {removed_task}")
-        else:
-            print("Invalid task number.")
 
-def main():
-    todo_list = ToDoList()
+def listTasks():
+  if not tasks:
+    print("There are no tasks currently.")
+  else:
+    print("Current Tasks:")
+    for index, task in enumerate(tasks):
+      print(f"Task #{index}. {task}")
 
-    while True:
-        print("\nTo-Do List Menu:")
-        print("1. Add task")
-        print("2. View tasks")
-        print("3. Delete task")
-        print("4. Exit")
 
-        choice = input("Enter your choice (1-4): ")
+def deleteTask():
+  listTasks()
+  try:
+    taskToDelete = int(input("Enter the # to delete: "))
+    if taskToDelete >= 0 and taskToDelete < len(tasks):
+      tasks.pop(taskToDelete)
+      print(f"Task {taskToDelete} has been removed.")
+    else:
+      print(f"Task #{taskToDelete} was not found.")
+  except:
+    print("Invalid input.")
 
-        if choice == '1':
-            task = input("Enter the task: ")
-            todo_list.add_task(task)
-        elif choice == '2':
-            todo_list.view_tasks()
-        elif choice == '3':
-            task_number = int(input("Enter the task number to delete: "))
-            todo_list.delete_task(task_number)
-        elif choice == '4':
-            print("Exiting the program.")
-            break
-        else:
-            print("Invalid choice. Please try again.")
 
 if _name_ == "_main_":
-    main()
+  ### Create a loop to run the app
+  print("Welcome to the to do list app :)")
+  while True:
+    print("\n")
+    print("Please select one of the following options")
+    print("------------------------------------------")
+    print("1. Add a new task")
+    print("2. Delete a task")
+    print("3. view tasks")
+    print("4. cancel")
+
+    choice = input("Enter your choice: ")
+
+    if (choice == "1"):
+      addTask()
+    elif (choice == "2"):
+      deleteTask()
+    elif (choice == "3"):
+      listTasks()
+    elif (choice == "4"):
+      break
+    else:
+      print("Invalid input. Please try again.")
+
+  print("exit")
